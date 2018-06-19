84bac195e0431ec977c0ad87a7b4c7793fcddbed
Calum Carey <calum@ayup.agency>
2018-06-19 14:49:30 +0100

    javscript additions and ggl map custom

--- js/main.js
+++ js/main.js
@@ -1,3 +1,47 @@
+// variables
+var row = document.getElementsByClassName('row'); // row variable
+var popup = document.getElementsByClassName('table__expand');
+var table = document.getElementsByClassName('table');
+var navText = document.getElementsByClassName('nav__item--text');
+// Attach event listeners to each row
+for(var i = 0 ; i < row.length; i++) {
+   row[i].addEventListener('click', toggleExpand);
+}
+
+//
+// Function to swtich the visibility state of the popup
+//
+function toggleExpand(e) {
+
+  // target row thats clicked
+  var selectedRow = e.target;
+
+/*   // create new tr and td upon click
+  var jsRow = document.createElement('tr');
+  var jsTd = document.createElement('td');
+
+  // attempted appening upon click but no luck
+  jsRow.appendChild(jsTd);
+  jsTd.append(popup[1]);
+*/
+  selectedRow.append(popup[0]);
+
+//Attempting to append via parent node of tr to <table>. attach the new tr to table. append new row to e.target
+
+  /* jsRow.ParentNode.append(jsTd);
+  jsTd.append(selectedRow); */
+
+  //appending popup to selected row. butnot */
+
+  //popup[0].classList.toggle('collapsed');
+   //array required to distinguish between .table__expand and .collapsed[1]
+
+}
+
+
+
+// CREATE X BUTTON TO CLOSE THE SCREEN .EG. ADD CLASS COLLAPSED TO POPUP
+
 // callback Initialise Map fucntion linked in script
 
 function initMap() {
@@ -6,8 +50,67 @@ function initMap() {
 
   var options = {
     zoom: 16,
-    center: {lat: 53.79595, lng: -1.54816}
-  }
+    center: {lat: 53.79595, lng: -1.54816},
+    disableDefaultUI: true,
+    styles: [
+      {elementType: 'geometry',stylers: [{color: '#EFD8B1'}]}, //background color - pear lusta
+      {
+        featureType: 'road', elementType: 'geometry', stylers: [{color: '#111716'}]
+      },
+      {
+        featureType: 'road', elementType: 'geometry.stroke', stylers: [{color: '#2D383A'}]
+      },
+
+      {
+       featureType: 'road',
+       elementType: 'labels.text.fill',
+       stylers: [{color: '#9ca5b3'}]
+     },
+     {
+       featureType: 'road.highway',
+       elementType: 'geometry',
+       stylers: [{color: '#746855'}]
+     },
+     {
+       featureType: 'road.highway',
+       elementType: 'geometry.stroke',
+       stylers: [{color: '#1f2835'}]
+     },
+     {
+       featureType: 'road.highway',
+       elementType: 'labels.text.fill',
+       stylers: [{color: '#f3d19c'}]
+     },
+     {
+       featureType: 'transit',
+       elementType: 'geometry',
+       stylers: [{color: '#2f3948'}]
+     },
+     {
+       featureType: 'transit.station',
+       elementType: 'labels.text.fill',
+       stylers: [{color: '#d59563'}]
+     },
+     {
+       featureType: 'water',
+       elementType: 'geometry',
+       stylers: [{color: '#17263c'}]
+     },
+     {
+       featureType: 'water',
+       elementType: 'labels.text.fill',
+       stylers: [{color: '#515c6d'}]
+     },
+     {
+       featureType: 'water',
+       elementType: 'labels.text.stroke',
+       stylers: [{color: '#17263c'}]
+     }
+
+    ]
+  };
+
+  //look into this a bit more
 
   //link ID to map
 
@@ -25,49 +128,70 @@ function initMap() {
 
 }
 
+// ----------  ^^^^^^^ MAP API ^^^^^^  --------------- //
 
-
-
-
-
-// variables
-var row = document.getElementsByClassName('row'); // row variable
-var popup = document.getElementsByClassName('table__expand');
-
-var table = document.getElementsByClassName('table');
-// Attach event listeners to each row
-for(var i = 0 ; i < row.length; i++) {
-   row[i].addEventListener('click', toggleExpand);
+for (var i = 0; i < navText.length; i++) {
+  navText[i].addEventListener('click', circle);
 }
 
-//
-// Function to swtich the visibility state of the popup
-//
-function toggleExpand(e) {
+function circle(e){
 
-  // target row thats clicked
-  var selectedRow = e.target.parentNode;
-
-  // create new tr and td upon click
-  var jsRow = document.createElement('tr');
-  var jsTd = document.createElement('td');
-
-  // attempted appening upon click but no luck
-  // jsRow.appendChild(jsTd);
-  //jsTd.append(popup[1]);
-
-  console.log(selectedRow.ParentNode);
-
-  selectedRow.ParentNode.append(jsRow);
-  jsRow.ParentNode.append(jsTd);
-  jsTd.append(popup[1]);
-  //appending popup to selected row. butnot
-
-  popup[0].classList.toggle('collapsed');
-   //array required to distinguish between .table__expand and .collapsed[1]
+  var selectedNav = e.target;
+  var circle = document.createElement('img');
+  circle.src = '/assets/media/shapes/nav_circle.svg';
+  // NEED TO ADD ALTERNATE AND SELECT VAR
+  selectedNav.appendChild(circle);
 
 }
 
+// ----- ^^^^^^^ ADD CIRCLE TO NAV ON CLICK ^^^^^ --------- //
+
+// ----------  PAGE BUTTON/SCROLL DIRECT ------------- //
 
 
-// CREATE X BUTTON TO CLOSE THE SCREEN .EG. ADD CLASS COLLAPSED TO POPUP
+/* var navLinks = document.getElementsByClassName('nav__items');
+
+for(var i = 0; i < navLinks.length; i++) {
+  navLinks[i].onclick = scroll;
+}
+
+function scroll(e) {
+
+  e.preventDefault();
+  var id = this.getAttribute('href').replace('#', ''); //maybe needs Space
+  var target = document.getElementById(id).getBoundingClientRect().top;
+  animateScroll(target);
+
+  function animateScroll(targetHeight) {
+    targetHeight = document.body.scrollHeight - window.innerHeight > targetHeight + scrollY ?
+        targetHeight : document.body.scrollHeight - window.innerHeight;
+    var initialPosition = window.scrollY;
+    var SCROLL_DURATION = 30;
+    var step_x = Math.PI / SCROLL_DURATION;
+    var step_count = 0;
+    requestAnimationFrame(step);
+    function step() {
+        if (step_count < SCROLL_DURATION) {
+            requestAnimationFrame(step);
+            step_count++;
+            window.scrollTo(0, initialPosition + targetHeight * 0.25 * Math.pow((1 - Math.cos(step_x * step_count)), 2));
+          }
+        }
+
+    }
+}
+
+*
+
+
+// MY ATTEMPT ----- >
+
+
+/* navBooking.addEventListener('click', function(){
+
+  var booking = document.getElementById('js-booking');
+  booking.scrollIntoView(true);
+  //console.log(booking);
+  // WORKS WITH BUTTONS
+});
+*/
